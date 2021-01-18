780a9aefaf435d2dfc979ac24c8e71e73320d3f4
talryan <nataliamryan@gmail.com>
2021-01-17 18:43:08 -0800

    Creating Sign Up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,21 @@ class SessionsController < ApplicationController
     def new
         @user = User.new
     end
-    
+
+    def create 
+        user = User.find_by(email:params['user']['email'])
+        if user && user.authenticate(params['user']['password'])
+            session[:user_id] = user.id 
+            redirect_to_user_path(user)
+        else 
+            flash[:message] = "Email or Password does not match our records. Please try again."
+            render :new
+        end
+    end    
+
+    def destroy
+        session.delete :user_id
+        flash[:message] = "You have Successfully Logged Out."
+        redirect_to root
+    end
 end
