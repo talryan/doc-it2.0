e1bb54f4386f21250beae5784f847be1589602a6
talryan <nataliamryan@gmail.com>
2021-01-17 18:25:18 -0800

    creating login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :appointments
   resources :patients
   resources :doctors
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
