fbd886a008520e3b352a5d2cb6c662e9918ee5f8
talryan <nataliamryan@gmail.com>
2021-01-17 18:03:21 -0800

    Adding model relationships.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    has_secure_password
     has_many :appointments
     has_many :doctors, through: :appointments
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, uniqueness: true, presence: true
 end
