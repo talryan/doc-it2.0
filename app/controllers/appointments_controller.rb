bc0a06aeba20af2e6a650714d343655516b40405
talryan <nataliamryan@gmail.com>
2021-01-18 16:16:31 -0800

    creating appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -5,6 +5,13 @@ class AppointmentsController < ApplicationController
     end
 
     def create 
+        appointment = Appointment.create(appointment_params)
     end
 
+
+    private 
+
+    def appointment_params
+        params.require(:appointment).permit(:time, :)
+    end
 end
