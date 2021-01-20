ff7ebb803644907c16877ecbe8c4020ec414e99f
talryan <nataliamryan@gmail.com>
2021-01-20 11:27:58 -0800

    doctor show page works

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,6 @@
 class Appointment < ApplicationRecord
   belongs_to :doctor
   belongs_to :user
+
+  accepts_nested_attributes_for :doctor
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :appointments
 
   resources :doctors do 
-    resources :appointments 
+    resources :appointments, shallow:true 
   end
 
 
