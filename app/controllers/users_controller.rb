37a9a9e3c6a3ad379ac6fb754063ab0b5306d0ef
talryan <nataliamryan@gmail.com>
2021-01-21 16:09:38 -0800

    Attempting to use scope method. Failing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
     end
 
     def show
-        
+        @user = User.find(params[:id]) 
+        @appointment = Appointment.find()
     end
 
     private 
