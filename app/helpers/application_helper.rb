adbba77e60827ea81da31dda00252c5166925b87
talryan <nataliamryan@gmail.com>
2021-01-22 16:54:32 -0800

    Scope Method added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,5 +7,5 @@ module ApplicationHelper
         @current_user ||=User.find_by_id(session[:user_id]) if session[:user_id]
     end
 
-
+   
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,13 @@
 module UsersHelper
 
-    #if user's upcomming appointments = 0, return "You have no upcomming appointments"
+#     if user's upcoming appointments = 0, return "You have no upcoming appointments"
+   def upcoming?
+        if Appointment.upcoming.empty?
+         "You have 0 upcoming appointments."
+        else 
+            "You have #{Appointment.upcoming.count} upcoming appointment(s)."
+        end
+    end
 # else 
-    #if user's upcoming appointments > 0, return "You have #{number of upcoming appointments} and list upcoming apppointments"
+#     if user's upcoming appointments > 0, return "You have #{number of upcoming appointments} and list upcoming appointments"
 end
