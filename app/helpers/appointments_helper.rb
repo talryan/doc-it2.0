48ec4c3920bf6a8367745c7cfee698373ac0e57e
talryan <nataliamryan@gmail.com>
2021-01-24 15:08:10 -0800

    Appointment#Show CSS.

--- app/helpers/appointments_helper.rb
+++ app/helpers/appointments_helper.rb
@@ -4,4 +4,13 @@ module AppointmentsHelper
         @appointment = Appointment.find(params[:id])
         @appointment.user_id == current_user.id
     end
+
+    def appointment_history?
+        if @appointment == nil 
+        "You have no appointment history."
+        else 
+            "You have #{@appointment.count} past appointment(s). "
+        end
+    end
 end
+
